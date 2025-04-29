389103d56ea0f93c2efbfd5eecba1e0ab9a493f3
Jeevananandan J <jeevasusej@gmail.com>
2025-04-29 22:08:48 +0530

    changed deployment based

--- vite.config.ts
+++ vite.config.ts
@@ -5,15 +5,18 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/bank-portal-test-suite/",
   server: {
     host: "::",
     port: 8080,
   },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
+  plugins: [react(), mode === "development" && componentTagger()].filter(
+    Boolean
+  ),
+  build: {
+    // change default "dist" to "build"
+    outDir: "build",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
