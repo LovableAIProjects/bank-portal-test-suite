0c6db51679a232bdf0eba78e5823b534a127b78c
Jeevananandan J <jeevasusej@gmail.com>
2025-04-29 22:31:52 +0530

    Base path modified

--- src/App.tsx
+++ src/App.tsx
@@ -13,7 +13,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/bank-portal-test-suite/">
         <Routes>
           <Route path="/" element={<Index />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
